d20ad9af6c6d2d710ea9eeaffe95bbe43cb7425f
Connor Bray <connorb108@gmail.com>
2024-03-13 03:20:07 +0000

    homepage flex

--- site/app/entry.client.tsx
+++ site/app/entry.client.tsx
@@ -4,15 +4,23 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { RemixBrowser } from '@remix-run/react';
+import { startTransition, StrictMode } from 'react';
+// import { hydrateRoot } from "react-dom/client";
+import { hydrate } from 'react-dom';
 
 startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
+	// hydrateRoot(
+	//   document,
+	//   <StrictMode>
+	//     <RemixBrowser />
+	//   </StrictMode>
+	// );
+	// Reverting to hydrate from react-dom to fix hydration issue. App uses react 17 instead of 18.
+	hydrate(
+		<StrictMode>
+			<RemixBrowser />
+		</StrictMode>,
+		document
+	);
 });

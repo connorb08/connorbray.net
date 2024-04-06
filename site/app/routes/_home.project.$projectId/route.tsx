6aefa76fa52549d6abc97d6e7dcd7307c0e5c70a
Connor Bray <connorb108@gmail.com>
2024-04-06 03:37:10 +0000

    remove coverage files

--- site/app/routes/_home.project.$projectId/route.tsx
+++ site/app/routes/_home.project.$projectId/route.tsx
@@ -1,7 +1,23 @@
-import { useParams } from '@remix-run/react';
+import { json } from '@remix-run/cloudflare';
+import { type LoaderFunctionArgs } from '@remix-run/cloudflare';
+import { useLoaderData, useParams } from '@remix-run/react';
 import ProjectOverview from '~/components/Projects/ProjectOverview';
 
+export async function loader({ params }: LoaderFunctionArgs) {
+	const res = await fetch(
+		'https://api.connorbray.net/projects/' + params.projectId
+	);
+
+	return json(
+		{
+			id: params.projectId,
+		} as Project,
+		{ headers: { 'Cache-Control': 'public, max-age=3600' } }
+	);
+}
+
 export default function Project() {
+	// const { name } = useLoaderData<typeof loader>();
 	const { projectId } = useParams();
 	return (
 		<ProjectOverview
